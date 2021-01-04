8632321a3a4c76bd0682ce9e2f51935dfce17db7
Watermelon <1050364782@qq.com>
2021-01-04 17:48:09 +0800

    demo v1.1 desc:built eureka cluster(2 node)

---
+++ distribute-eureka-02/src/main/java/org/watermelon/EurekaServerApplication_02.java
@@ -0,0 +1,15 @@
+package org.watermelon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication_02 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication_02.class,args);
+    }
+
+}
