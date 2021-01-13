866e13968354f5205ab538a5aeaa6483f0b28507
Watermelon <1050364782@qq.com>
2021-01-13 17:32:00 +0800

    demo v1.9 desc:import config-client module,change zuul module port

---
+++ distribute-config-client/src/main/java/org/watermelon/DistributeConfigClientApplication.java
@@ -0,0 +1,13 @@
+package org.watermelon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributeConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributeConfigClientApplication.class,args);
+    }
+
+}
